4fa965005d4008cfcfc9e10705af483c5b2d2ec2
cheroliv <cheroliv@laposte.net>
2025-12-11 01:17:59 +0100

    ✨ (refactor)(FileSystemManager): Simplify CNAME file creation logic

--- bakery-plugin/bakery/src/main/kotlin/com/cheroliv/bakery/FileSystemManager.kt
+++ bakery-plugin/bakery/src/main/kotlin/com/cheroliv/bakery/FileSystemManager.kt
@@ -3,7 +3,6 @@ package com.cheroliv.bakery
 import com.cheroliv.bakery.GitService.FileOperationResult
 import com.cheroliv.bakery.GitService.FileOperationResult.Failure
 import com.cheroliv.bakery.GitService.FileOperationResult.Success
-import com.cheroliv.bakery.RepositoryConfiguration.Companion.CNAME
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
@@ -13,7 +12,6 @@ import org.gradle.api.GradleException
 import org.gradle.api.Project
 import org.slf4j.Logger
 import java.io.File
-import java.io.File.separator
 import java.io.IOException
 import kotlin.text.Charsets.UTF_8
 
@@ -21,33 +19,33 @@ object FileSystemManager {
 
     // Publishing logic
     fun createRepoDir(path: String, logger: Logger): File = path.let(::File).apply {
-            if (exists() && !isDirectory) if (delete()) logger.info("$name exists as file and successfully deleted.")
-            else throw "$name exists and must be a directory".run(::IOException)
+        if (exists() && !isDirectory) if (delete()) logger.info("$name exists as file and successfully deleted.")
+        else throw "$name exists and must be a directory".run(::IOException)
 
-            if (exists()) if (deleteRecursively()) logger.info("$name exists as directory and successfully deleted.")
-            else throw "$name exists as a directory and cannot be deleted".run(::IOException)
+        if (exists()) if (deleteRecursively()) logger.info("$name exists as directory and successfully deleted.")
+        else throw "$name exists as a directory and cannot be deleted".run(::IOException)
 
-            if (!exists()) logger.info("$name does not exist.")
-            else throw IOException("$name must not exist anymore.")
+        if (!exists()) logger.info("$name does not exist.")
+        else throw IOException("$name must not exist anymore.")
 
-            if (!exists()) {
-                if (mkdir()) logger.info("$name as directory successfully created.")
-                else throw IOException("$name as directory cannot be created.")
-            }
+        if (!exists()) {
+            if (mkdir()) logger.info("$name as directory successfully created.")
+            else throw IOException("$name as directory cannot be created.")
         }
+    }
 
     fun copyBakedFilesToRepo(
         bakeDirPath: String, repoDir: File, logger: Logger
     ): FileOperationResult = try {
         bakeDirPath.also { "bakeDirPath : $it".let(logger::info) }.let(::File).apply {
-                copyRecursively(repoDir, true)
-                deleteRecursively()
-            }.run {
-                when {
-                    !exists() -> logger.info("$name directory successfully deleted.")
-                    else -> throw IOException("$name must not exist.")
-                }
+            copyRecursively(repoDir, true)
+            deleteRecursively()
+        }.run {
+            when {
+                !exists() -> logger.info("$name directory successfully deleted.")
+                else -> throw IOException("$name must not exist.")
             }
+        }
         Success
     } catch (e: Exception) {
         Failure(e.message ?: "An error occurred during file copy.")
@@ -70,33 +68,14 @@ object FileSystemManager {
 
 
     fun SiteConfiguration.createCnameFile(project: Project) {
-        if (bake.cname != null && bake.cname.isNotBlank()) project.file(
-            "${project.layout.buildDirectory.get().asFile.absolutePath}${
-                separator
-            }${
-                bake.destDirPath
-            }${separator}${CNAME}"
-        ).apply {
-            parentFile.mkdirs()
-        }.run {
-            if (exists() && isDirectory) {
-                project.logger.info("$name exists as directory.")
-                deleteRecursively()
-            } else project.logger.info("$name does not exist as directory.")
-            if (exists()) {
-                project.logger.info("$name successfully deleted.")
-                delete()
-            } else project.logger.info("$name does not exist as file.")
-            if (exists()) throw "$name file should not exist.".run(::IOException)
-            else project.logger.info("$name does not exist as file or directory.")
-            if (!createNewFile()) throw "Cannot create $name file.".run(::IOException)
-            else project.logger.info("$name successfully created.")
-            if (!exists()) throw "$name file should exist.".run(::IOException)
-            else project.logger.info("$name file exists.")
-            appendText(bake.cname ?: "", UTF_8)
-            if (!(exists() && !isDirectory)) throw "$name file should exist as file not as directory.".run(::IOException) else project.logger.info(
-                "$name file exists as file and not directory."
-            )
+        val cnameFile: File = project.layout.buildDirectory.get()
+            .asFile
+            .resolve(bake.destDirPath)
+            .resolve("CNAME")
+        if (cnameFile.exists()) cnameFile.delete()
+        if (!bake.cname.isNullOrBlank()) {
+            cnameFile.createNewFile()
+            cnameFile.writeText(bake.cname, UTF_8)
         }
     }
 
