ca1fd14b0715d241dd00d9752dc25be18485cd7f
cheroliv <cheroliv@laposte.net>
2025-12-13 03:36:00 +0100

    ✨ feat(deps): Update bakery plugin to 0.0.7 and clean up test dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,27 +12,6 @@ val jbakeRuntime: Configuration by configurations.creating {
 }
 
 dependencies {
-    testRuntimeOnly(libs.junit.platform.launcher)
-    testImplementation(libs.kotlinx.coroutines.core)
-    testImplementation(libs.ktor.client)
-    testImplementation(libs.playwright)
-    testImplementation(libs.supabase.postgrest)
-    testImplementation(libs.supabase.storage)
-    testImplementation(libs.supabase.jackson)
-    testImplementation(libs.supabase.auth)
-    testImplementation(libs.supabase.auth.jvm)
-
-    listOf(
-        kotlin("test"),
-        gradleTestKit(),
-        libs.junit.jupiter,
-        libs.jackson.module.kotlin,
-        libs.jackson.dataformat.yaml,
-        libs.jackson.datatype.jsr310,
-        libs.jackson.module.jsonSchema,
-        libs.jackson.databind.yaml,
-    ).forEach { testImplementation(it) }
-
     jbakeRuntime("org.jbake:jbake-core:2.6.7")
 
     arrayOf(
@@ -42,58 +21,48 @@ dependencies {
     ).map { jbakeRuntime(it) }
 }
 
-//tasks.register<JavaExec>("serve") {
-//    group = "bakery"
-//    description = "Serves the baked site locally."
+/**
+ * TODO: acces to SiteConfiguration programmatically, and add to plugin
+ */
+tasks.register<JavaExec>("serve") {
+    group = "bakery"
+    description = "Serves the baked site locally."
 //    val site: SiteConfiguration = readSiteConfiguration(
 //        project,
 //        project.file(bakery.configPath)
 //    )
-//    mainClass.set("org.jbake.launcher.Main")
-//    classpath = jbakeRuntime
-//    environment(
-//        "GEM_PATH",
-//        configurations.getByName("jbakeRuntime").asPath
-//    )
-//    jvmArgs(
-//        "--add-opens=java.base/java.lang=ALL-UNNAMED",
-//        "--add-opens=java.base/java.util=ALL-UNNAMED",
-//        "--add-opens=java.base/java.io=ALL-UNNAMED",
-//        "--add-opens=java.base/sun.nio.ch=ALL-UNNAMED"
-//    )
-//    args = listOf(
-//        "-b",
+    mainClass.set("org.jbake.launcher.Main")
+    classpath = jbakeRuntime
+    environment(
+        "GEM_PATH",
+        configurations.getByName("jbakeRuntime").asPath
+    )
+    jvmArgs(
+        "--add-opens=java.base/java.lang=ALL-UNNAMED",
+        "--add-opens=java.base/java.util=ALL-UNNAMED",
+        "--add-opens=java.base/java.io=ALL-UNNAMED",
+        "--add-opens=java.base/sun.nio.ch=ALL-UNNAMED"
+    )
+
+    args = listOf(
+        "-b",
 //        site.bake.srcPath
-//            .run(project::file)
-//            .absolutePath,
-//        "-s",
+        "site"
+            .run(project::file)
+            .absolutePath,
+        "-s",
 //        site.bake.destDirPath
-//            .run(project.layout.buildDirectory.get().asFile::resolve)
-//            .absolutePath,
-//    )
-//    doFirst {
-//        "Serving $group at: https://localhost:8820/"
-//            .run(::println)
-//    }
-//}
+        "bake"
+            .run(project.layout.buildDirectory.get().asFile::resolve)
+            .absolutePath,
+    )
+    doFirst {
+        "Serving $group at: https://localhost:8820/"
+            .run(::println)
+    }
+}
 
 tasks.named<Wrapper>("wrapper") {
     gradleVersion = libs.gradle.tooling.api.get().version
     distributionType = BIN
 }
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-//    dependsOn("serve")
-//    finalizedBy("stopServe")
-    jvmArgs(
-        "--add-opens=java.base/java.lang=ALL-UNNAMED",
-        "--add-opens=java.base/java.util=ALL-UNNAMED",
-        "--add-opens=java.base/java.io=ALL-UNNAMED"
-    )
-    systemProperty("project.root.dir", project.rootDir.absolutePath)
-    systemProperty("playwright.headless", "false")
-    testLogging { showStandardStreams = true }
-}
-
-val functionalTest by sourceSets.creating
