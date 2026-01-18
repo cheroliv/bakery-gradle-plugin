ee4d6d5dcb281e53554b1449c77573e626fe4318
cheroliv <cheroliv@laposte.net>
2026-01-18 21:56:56 +0100

    ✨ refactor(build): Remove commented code and unused dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,3 @@
-//import org.gradle.api.tasks.wrapper.Wrapper.DistributionType.BIN
-
 plugins { alias(libs.plugins.bakery) }
 
 bakery { configPath = file("site.yml").absolutePath }
-
-//tasks.named<Wrapper>("wrapper") {
-//    gradleVersion = libs.gradle.tooling.api.get().version
-//    distributionType = BIN
-//}
