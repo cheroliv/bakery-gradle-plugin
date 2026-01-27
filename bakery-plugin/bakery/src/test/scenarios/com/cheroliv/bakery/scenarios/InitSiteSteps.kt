6d9c4458e18e9b53b75d8ac173611ac19c7e7d8a
cheroliv <cheroliv@laposte.net>
2026-01-27 18:30:35 +0100

    ✨ feat(test): Refactor buildScript assertion in InitSiteSteps

--- bakery-plugin/bakery/src/test/scenarios/com/cheroliv/bakery/scenarios/InitSiteSteps.kt
+++ bakery-plugin/bakery/src/test/scenarios/com/cheroliv/bakery/scenarios/InitSiteSteps.kt
@@ -2,16 +2,17 @@ package com.cheroliv.bakery.scenarios
 
 import io.cucumber.java.en.And
 import org.assertj.core.api.Assertions.assertThat
+import kotlin.text.Charsets.UTF_8
 
 class InitSiteSteps(private val world: TestWorld) {
 
     @And("I add a buildScript file with {string} as the config path in the dsl")
     fun checkBuildScript(configfile: String) {
-        assertThat(
-            world.projectDir!!
-                .resolve("build.gradle.kts")
-                .readText()
-        ).describedAs("Should contains plugins block and bakery dsl.")
+        "build.gradle.kts"
+            .run(world.projectDir!!::resolve)
+            .readText(UTF_8)
+            .run(::assertThat)
+            .describedAs("Gradle buildScript should contains plugins block and bakery dsl.")
             .contains(
                 "plugins { id(\"com.cheroliv.bakery\") }",
                 "bakery { configPath = file(\"site.yml\").absolutePath }"
